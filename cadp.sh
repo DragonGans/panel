9d4fbdbac56e4151250886205bd5035c23e69372
Zx77 <85856958+DragonGans@users.noreply.github.com>
2025-08-15 19:25:52 +0700

    Create cadp.sh

---
+++ cadp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Script buat admin Pterodactyl (versi baru)
+# Pastikan jalankan di folder instalasi panel (misal /var/www/pterodactyl)
+
+PT_PATH="/var/www/pterodactyl"   # Lokasi Pterodactyl
+EMAIL="ryoo@example.com"         # Email admin
+USERNAME="ryoo"
+PASSWORD="ryoo1221"
+FIRST_NAME="Ryoo"
+LAST_NAME="Admin"
+
+cd "$PT_PATH" || { echo "❌ Folder $PT_PATH tidak ditemukan"; exit 1; }
+
+# Jalankan artisan untuk membuat user admin
+php artisan p:user:make \
+    --email="$EMAIL" \
+    --username="$USERNAME" \
+    --name-first="$FIRST_NAME" \
+    --name-last="$LAST_NAME" \
+    --password="$PASSWORD" \
+    --admin=1
+    -q
+
+echo "✅ Admin berhasil dibuat!"
+echo "Username : $USERNAME"
+echo "Password : $PASSWORD"
+echo "Email    : $EMAIL"
